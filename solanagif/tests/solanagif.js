bb50eaa86bf24cdece29b309d0c656459eac624a
Tapabrata Dey <tapabratadey02@gmail.com>
2021-11-13 05:18:10 -0500

    push back

--- solanagif/tests/solanagif.js
+++ solanagif/tests/solanagif.js
@@ -1,18 +1,52 @@
 // import anchor
 const anchor = require('@project-serum/anchor');
 
+// import SystemProgram
+const { SystemProgram } = anchor.web3;
+
 // run main asynchronously
 const main = async () => {
   console.log('🚀 Starting test...');
+
+  // create and set the provider.
   // letting Anchor get local environment to run code locally
-  anchor.setProvider(anchor.Provider.env());
+  // and communicate with the frontend
+  const provider = anchor.Provider.env();
+  anchor.setProvider(provider);
+
   // run rust program
   const program = anchor.workspace.Solanagif;
+
+  // create an account keypair for Solana program to use.
+  const baseAccount = anchor.web3.Keypair.generate();
+
   // local validator will mine the instructions in
   // startStuffOff() so we await it
-  const tx = await program.rpc.startStuffOff();
+  const tx = await program.rpc.startStuffOff({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: SystemProgram.programId,
+    },
+    signers: [baseAccount],
+  });
 
   console.log('📝 Your transaction signature', tx);
+
+  // fetch data from the account and access totalGifs
+  let account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString());
+
+  // add a new GIF
+  await program.rpc.addGif({
+    accounts: {
+      baseAccount: baseAccount.publicKey,
+    },
+  });
+
+  // fetch totalGifs again
+  account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+  console.log('👀 GIF Count', account.totalGifs.toString());
 };
 
 const runMain = async () => {
